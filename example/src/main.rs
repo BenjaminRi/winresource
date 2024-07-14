4f0e8d163bd97a56b5292a13455de71c81efe797
inklesspen <inklesspen@yandex.ru>
2024-07-15 02:59:46 +0300

    Make example work on unix and ever

--- example/src/main.rs
+++ example/src/main.rs
@@ -1,17 +1,6 @@
-extern crate winapi;
-
-use std::ffi::CString;
-use winapi::um::winuser::{MessageBoxA, MB_ICONINFORMATION, MB_OK};
+use std::io::stdin;
 
 fn main() {
-    let lp_text = CString::new("Hello, world!").unwrap();
-    let lp_caption = CString::new("MessageBox Example").unwrap();
-    unsafe {
-        MessageBoxA(
-            std::ptr::null_mut(),
-            lp_text.as_ptr(),
-            lp_caption.as_ptr(),
-            MB_OK | MB_ICONINFORMATION,
-        );
-    }
+    println!("Hello winresource!");
+    stdin().lines().next();
 }
